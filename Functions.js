3ab51b0304fcb9077e323433ff52ed664deedf12
ElenaPogudo <elena@pogudo.org>
2018-07-03 23:00:08 +0300

    some problems with id search

--- Functions.js
+++ Functions.js
@@ -6,38 +6,44 @@ const firstUrl = "https://rickandmortyapi.com/api/character";
 function dataReceiver() {
     let result = [];
     const getChars = (firstUrl) => request(firstUrl, {json: true}).then((body) => {
-            result = result.concat(body.results);
-            if (body.info.next !== "") {
-                return getChars(body.info.next);
-            } else return result;
-        });
+        result = result.concat(body.results);
+        if (body.info.next !== "") {
+            return getChars(body.info.next);
+        } else return result;
+    });
 
     return getChars(firstUrl);
 }
 
 function search(args) {
     return new Promise((resolve, reject) => {
-        const properties = getProperties(args);               
+        const properties = getProperties(args);
         try {
-            dataReceiver().then((data) => {        
+            dataReceiver().then((data) => {
                 const chars = data.map((el) => {
-                    let flag = true;                       //flag to indicate that current character fits the criteria
-                    for (let i = 0; i < properties.length; i++) {                         //checking multiple parameters
-                        if (properties[i] === 'origin' || properties[i] === 'location') {       //"origin" and "location" parameters are objects
-                            if (el[properties[i]].name.indexOf(args[properties[i]]) === -1) {   //and have to be checked by their .name property
-                                flag = false;              //if one parameter does not fit the criteria, character is marked as not right
-                                break;                     //and cycle breaks
+                    let addToRes = true;
+                    for (let i = 0; i < properties.length; i++) {
+                        if (properties[i] === 'origin' || properties[i] === 'location') {
+                            if (el[properties[i]].name.indexOf(args[properties[i]]) === -1) {
+                                addToRes = false;
+                                break;
                             }
-                        } else {
+                        } else if (properties[i] === 'id') {
+                            if (el.id !== args['id']) { // im not sure why but id from console read like bool true
+                                console.log(args['id']);
+                                addToRes = false;
+                            }
+                        }
+                        else {
                             if (el[properties[i]].indexOf(args[properties[i]]) === -1) {
-                                flag = false;
+                                addToRes = false;
                                 break;
                             }
                         }
                     }
-                    if (flag === true) {
+                    if (addToRes === true) {
                         return el;
-                    }       //if the character is marked as right, return it to the result array
+                    }
                 })
                 resolve(chars);
             })
